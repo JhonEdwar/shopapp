46737c7dc6e111f48bce1630a40faf526e63fa7b
Jhon Edwar <jhonedwar192@gmail.com>
2023-02-04 19:36:34 -0500

    create folders and files

---
+++ src/screens/categories/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+import { colors } from "../../constants/index";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: colors.white,
+    paddingVertical:20,
+}});
---
+++ src/screens/products-detail/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import { styles } from "./styles";
+
+const ProductsDetail = () => {
+  return (
+    <View style={styles.container}>
+      <Text>ProductsDetail</Text>
+    </View>
+  );
+};
+
+export default ProductsDetail;
