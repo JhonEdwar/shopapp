81b06ac966613216bc0d7800eb8c46d9b0a5b15d
Jhon Edwar <jhonedwar192@gmail.com>
2023-02-12 15:54:55 -0500

    add icons

--- src/navigation/tabs.jsx
+++ src/navigation/tabs.jsx
@@ -4,17 +4,67 @@ import CartNavigator from "./cart";
 import OrdersNavigator from "./orders";
 import ShopNavigator from "./shop";
 import { THEME } from "../constants/theme";
+import Ionicons from '@expo/vector-icons/Ionicons'
 
-const BottomTab = createBottomTabNavigator()
+const BottomTab = createBottomTabNavigator();
 
-const Tabs=()=>{
-    return(
-        <BottomTab.Navigator initialRouteName="ShopTab">
-            <BottomTab.Screen name='ShopTab' component={ShopNavigator}/>
-            <BottomTab.Screen name='OrderTab' component={OrdersNavigator}/>
-            <BottomTab.Screen name='CartTab' component={CartNavigator}/>
-        </BottomTab.Navigator>
-    )
-}
+const Tabs = () => {
+  return (
+    <BottomTab.Navigator
+      initialRouteName="ShopTab"
+      screenOptions={{
+        headerShown: false,
+        tabBarLabelStyle: {
+          fontFamily: "SignikaNegative-Medium",
+          fontSize: 12,
+        },
+        tabBarActiveTintColor: THEME.colors.primary,
+        tabBarInactiveTintColor: THEME.colors.gray,
+      }}>
+      <BottomTab.Screen
+        name="ShopTab"
+        component={ShopNavigator}
+        options={{
+          title: "Shop",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              size={22}
+              color={THEME.colors.primary}
+            />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="OrderTab"
+        component={OrdersNavigator}
+        options={{
+          title: "Orders",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "file-tray" : "file-tray-outline"}
+              size={22}
+              color={THEME.colors.primary}
+            />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="CartTab"
+        component={CartNavigator}
+        options={{
+          title: "Cart",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "cart" : "cart-outline"}
+              size={22}
+              color={THEME.colors.primary}
+            />
+          ),
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+};
 
-export default Tabs
+export default Tabs;
